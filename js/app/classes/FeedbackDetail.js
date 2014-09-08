784eee00c64180e1c5d9f6bda03a9b16df4b5f65
Tiago Romero Garcia <tiagoromerogarcia@gmail.com>
2014-09-07 21:35:49 -0700

    feedback list and item working

--- js/app/classes/FeedbackDetail.js
+++ js/app/classes/FeedbackDetail.js
@@ -7,25 +7,26 @@ define([ 'jquery', 'backbone', 'react', 'react.backbone' ],
     var FeedbackItem = React.createBackboneClass({
       render: function () {
         var photoURL = this.getModel().get('photo') || defaultPhoto,
+            date = new Date( this.getModel().get('time') ).toLocaleString(),
             questionsItems = this.getModel().get('questions').map(function(question) {
             return (
-              React.DOM.li(null, 
-                React.DOM.h3(null,  question.title), 
+              React.DOM.li({className: "question"}, 
+                React.DOM.div({className: "question-title"},  question.title), 
                  question.answer
               )
             );
         });
 
         return (
-          React.DOM.section(null, 
+          React.DOM.section({className: "feedback-detail"}, 
             React.DOM.header(null, 
-              React.DOM.img({src: "{ photoURL }", className: "photo"}), 
-              React.DOM.div({className: "title"},  this.getModel().get('name'), "'s feedback at ",  this.getModel('time'))
+              React.DOM.img({src: photoURL, className: "photo"}), 
+              React.DOM.h2(null,  this.getModel().get('name'), "'s feedback at ", date )
             ), 
             React.DOM.ol(null, 
               questionsItems 
             ), 
-            React.DOM.a({href: "/"}, "Back to feedbacks")
+            React.DOM.a({href: "#"}, "Back to feedbacks")
           )
         );
       }
--- js/app/classes/FeedbackItem.jsx
+++ js/app/classes/FeedbackItem.jsx
@@ -8,10 +8,11 @@ define([ 'jquery', 'backbone', 'react', 'react.backbone' ],
         render: function () {
           var photoURL = this.getModel().get('photo') || defaultPhoto,
               date = new Date( this.getModel().get('time') ).toLocaleString(),
-              feedbackURL = '/feedback/' + this.getModel().get('id');
+              feedbackURL = '#feedback/' + this.getModel().get('id'),
+              itemKey = 'feedback-item-' + this.getModel().get('id');
 
           return (
-            <li className="list-group-item feedback-item">
+            <li key={ itemKey } className="list-group-item feedback-item">
               <img src={ photoURL } className="photo"/>
               <div className="description">{ this.getModel().get('name') } gave a feedback at { date }</div>
               <a href={ feedbackURL }>Read feedback</a>
